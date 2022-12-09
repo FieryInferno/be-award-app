27ecd2b766e087688af91a0e12b6300e65894e74
FieryInferno <bagassetia271@gmail.com>
2022-12-09 14:23:56 +0700

    [AWARD] Login Process

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+dotenv.config();
+
+const url = process.env.URL;
+
+app.use(cors({origin: url}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
+app.get('/', (req, res) => res.json({
+  message: 'Welcome to Award API',
+}));
